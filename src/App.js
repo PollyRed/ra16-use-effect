8025400aa95f0ecf27e3bc8a832b992a17e2f756
Polina Titova <titova_polina@mail.ru>
2022-07-31 00:34:35 +0300

    Task is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import List from './components/List';
+import Details from './components/Details';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [users, setUsers] = useState([]);
+    const [id, setId] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+    const url = 'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json';
+
+    useEffect(() => {
+        try {
+            fetch(url)
+                .then(response => response.json())
+                .then((users) => {
+                    setLoading(false);
+                    setUsers(users)
+                })
+
+        } catch (e) {
+            console.error(e);
+        }
+
+    }, []);
+
+    const viewDetails = (id) => {
+        setId(id);
+    }
+
+
+   return (
+     <div className="App">
+        {isLoading ? <div>Loading...</div> : <List users={users} viewDetails={viewDetails}/>}
+      <Details id={id}/>
+     </div>
+   );
 }
 
 export default App;
